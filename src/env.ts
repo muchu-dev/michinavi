2fb1ebabcd5359519e2b49f5156b388fcf5bb1b1
gesop0n <ishikuro6.2@gmail.com>
2026-08-22 23:22:00 +0900

    Update env.ts

--- src/env.ts
+++ src/env.ts
@@ -5,15 +5,16 @@ export const env = createEnv({
   // Client Component
   client: {
     NEXT_PUBLIC_SUPABASE_URL: z.url(),
-    NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY: z.string()
+    NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY: z.string(),
   },
   // Server Component / Route Handler
   server: {
-    APP_ENV: z.enum(["production", "preview", "local"])
+    APP_ENV: z.enum(["production", "preview", "local"]),
   },
   runtimeEnv: {
     NEXT_PUBLIC_SUPABASE_URL: process.env.NEXT_PUBLIC_SUPABASE_URL,
-    NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY: process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY,
-    APP_ENV: process.env.APP_ENV
-  }
-})
+    NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY:
+      process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY,
+    APP_ENV: process.env.APP_ENV,
+  },
+});
